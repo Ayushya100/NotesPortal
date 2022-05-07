5769d15b159011ee8208fca0ba419e6bd763fc0d
Ayushya <jaiswalayushya@gmail.com>
2022-05-07 18:25:28 +0530

    Added getNotesByIdAPI

--- serverSide/src/app.js
+++ serverSide/src/app.js
@@ -18,8 +18,10 @@ const api = '/api/v1/notes'
 
 // Routes
 const getAllNotes = require('../routes/getAllNotes');
+const getNoteById = require('../routes/getNotesById');
 
 app.use(`${api}/getAllNotes`, getAllNotes);
+app.use(`${api}/getNoteById`, getNoteById);
 
 app.listen(port, () => {
     console.log(`Connection has started at port: ${port}`);
